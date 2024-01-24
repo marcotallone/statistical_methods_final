dfcd5f3fac9000080a9edf73c23c8fd6b7e3e45f
marco <marco.tallone@studenti.units.it>
2024-01-24 18:18:06 +0100

    Experiments joining classes, factoring integers counts on logistic reg.

--- r_scripts/logistic_regression.r
+++ r_scripts/logistic_regression.r
@@ -2,6 +2,7 @@
 library(car)
 library(caret)
 library(pROC)
+library(forcats)
 
 # FUNCTIONS --------------------------------------------------------------------
 
@@ -184,12 +185,25 @@ bank <- read.csv("../datasets/BankChurners.csv", sep = ",")
 bank <- bank[, -c(1, 3, 5, 6, 9, 10, 14, 16, 17, 21, 22, 23)]
 bank$Attrition_Flag <- ifelse(bank$Attrition_Flag == "Attrited Customer", 1, 0)
 
-# Convert all categorical variables to factors
+# Convert all categorical variables to factors and reorder the levels
 bank$Gender <- as.factor(bank$Gender)
 bank$Marital_Status <- as.factor(bank$Marital_Status)
-bank$Income_Category <- as.factor(bank$Income_Category)
+bank$Marital_Status <- forcats::fct_relevel(bank$Marital_Status,
+                                            "Unknown",
+                                            "Single",
+                                            "Married",
+                                            "Divorced")
 
-# Override the Total_Trans_Amt variable with its log
+bank$Income_Category <- as.factor(bank$Income_Category)
+bank$Income_Category <- forcats::fct_relevel(bank$Income_Category,
+                                             "Unknown",
+                                             "Less than $40K",
+                                             "$40K - $60K",
+                                             "$60K - $80K",
+                                             "$80K - $120K",
+                                             "$120K +")
+
+# Override the Total_Trans_Amt variable with its log !!!
 bank$Total_Trans_Amt <- log(bank$Total_Trans_Amt)
 
 # Standardization (optional) all columns except response and categorical
@@ -212,3 +226,62 @@ cv(bank)
 
 anova(bank_logistic, test = "Chisq")
 vif(bank_logistic)
+
+# ------------------------------------------------------------------------------
+# MODIFIED DATASET -------------------------------------------------------------
+
+# # Chenaging the levels of Income_Category in less than 80K or more than 80K
+# bank$Income_Category <- fct_collapse(bank$Income_Category,
+#                                      "Less than 80K" = c("Less than $40K",
+#                                                          "$40K - $60K",
+#                                                          "$60K - $80K"),
+#                                      "More than 80K" = c("$80K - $120K",
+#                                                          "$120K +"))
+
+# Changing the levels of income category into a binary variable:
+bank$Income_Category <- fct_collapse(bank$Income_Category,
+                                     "Less than 120K" = c("Unknown",
+                                                          "Less than $40K",
+                                                          "$40K - $60K",
+                                                          "$60K - $80K",
+                                                          "$80K - $120K"),
+                                     "More than 120K" = c("$120K +"))
+
+# Chnanging the levels of Marital_Status in either married or not married
+bank$Marital_Status <- fct_collapse(bank$Marital_Status,
+                                    "Married" = c("Married"),
+                                    "Not Married" = c("Divorced",
+                                                      "Single",
+                                                      "Unknown"))
+
+# Converting Months_Inactive_12_mon to a factor
+bank$Months_Inactive_12_mon <- as.factor(bank$Months_Inactive_12_mon)
+levels(bank$Months_Inactive_12_mon) <- c("0", "1", "2", "3", "4", "5", "6+")
+
+# Joining toghether the levels after 4 months
+bank$Months_Inactive_12_mon <- fct_collapse(bank$Months_Inactive_12_mon,
+                                            "4+" = c("4", "5", "6+"))
+
+# # Converting Contacts_Count_12_mon to a factor
+# bank$Contacts_Count_12_mon <- as.factor(bank$Contacts_Count_12_mon)
+# levels(bank$Contacts_Count_12_mon) <- c("0", "1", "2", "3", "4", "5", "6+")
+
+# # Joining toghether the levels after 4 contacts
+# bank$Contacts_Count_12_mon <- fct_collapse(bank$Contacts_Count_12_mon,
+#                                            "4+" = c("4", "5", "6+"))
+
+# Removing from the dataset the Marital_Status and Income_Category variables
+bank <- bank[, -c(3, 4)]
+
+# Rebuiding and checking the model
+bank_logistic <- learn(bank)
+summary(bank_logistic)
+anova(bank_logistic, test = "Chisq")
+vif(bank_logistic)
+
+# Assessing the model
+cat("Results on whole dataset:\n")
+results <- assess(bank_logistic, bank)
+
+cat("Results on 10-fold cross validation:\n")
+cv(bank)
