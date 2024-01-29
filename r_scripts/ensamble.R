8bfda63aba7d1e4a123c735c15826d13d73848c3
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-01-29 11:57:30 +0100

    Added study on the whole dataset to see if the reduced one has similar accuracy

--- r_scripts/ensamble.R
+++ r_scripts/ensamble.R
@@ -5,7 +5,7 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 # Load the dataset and pre-process it
 bank <- read.csv("../datasets/BankChurners.csv", sep = ",")
-bank <- bank[, -c(1, 3, 5, 6, 9, 10, 14, 16, 17, 21, 22, 23)]
+bank <- bank[, -c(1, 22, 23)]
 #bank$Attrition_Flag <- ifelse(bank$Attrition_Flag == "Attrited Customer", 1, 0)
 # Convert Attrition_Flag to a binary factor
 bank$Attrition_Flag <- factor(bank$Attrition_Flag == "Attrited Customer", levels = c(FALSE, TRUE))
@@ -36,6 +36,11 @@ levels(bank$Months_Inactive_12_mon) <- c("0", "1", "2", "3", "4", "5", "6+")
 # Joining toghether the levels after 4 months
 bank$Months_Inactive_12_mon <- fct_collapse(bank$Months_Inactive_12_mon,
                                             "4+" = c("4", "5", "6+"))
+
+bank$Education_Level <- as.factor(bank$Education_Level)
+bank$Marital_Status <- as.factor(bank$Marital_Status)
+bank$Income_Category <- as.factor(bank$Income_Category)
+bank$Card_Category <- as.factor(bank$Card_Category)
 # Override the Total_Trans_Amt variable with its log !!!
 bank$Total_Trans_Amt <- log(bank$Total_Trans_Amt)
 # Standardization (optional) all columns except response and categorical
@@ -367,6 +372,8 @@ cv <- function(data, k = 10) {
 
 
 #-------------------------------------------------------------------------------
+# Results with full dataset
+#-------------------------------------------------------------------------------
 # IMPORT LIBRARIES
 library(randomForest)
 library(caret)
@@ -432,7 +439,120 @@ print(bank.rf)
 varImpPlot(bank.rf, sort = TRUE, n.var = 10, main = "Variable Importance")
 
 # Variable importance table
-importance(bank.rf, type = 2)
+# Extract variable importance
+variable_importance <- importance(bank.rf, type = 2)
+
+# Create a data frame for variable names and importance values
+variable_importance_df <- data.frame(
+  Variable = row.names(variable_importance),
+  Importance = as.numeric(variable_importance),
+  row.names = NULL
+)
+
+# Sort the data frame by Importance in descending order
+variable_importance_df <- variable_importance_df[order(-variable_importance_df$Importance), ]
+
+# Print the variable importance as a data frame
+print(variable_importance_df)
+
+
+# Plotting the tree
+plot(bank.rf, main = "Random Forest")
+legend("topright", colnames(bank.rf$err.rate), col = 1:3, fill = 1:3)
+
+#-------------------------------------------------------------------------------
+# ASSESSMENT
+cat("Results on whole dataset:\n")
+# AdaBoost
+boost.results <- assess.boost(bank.boost, test.bank)
+boost.resultscv <- cv.boost(bank, k = 10)
+
+# Random Forest
+rf.results <- assess(bank.rf, test.bank)
+rf.resultscv <- cv(bank, k = 10)
+
+#-------------------------------------------------------------------------------
+# Results with reduced dataset
+bank
+bank <- bank[, -c(2, 4, 5, 8, 9, 13, 15, 16, 20)]
+cat("Results on reduced dataset:\n")
+
+# FIRST TRY WITH BOOSTING
+# Static splitting the dataset into the Training set and Test set
+set.seed(1234)
+index <- createDataPartition(bank$Attrition_Flag , p =0.8, list = FALSE)
+
+train.bank <- bank[index,]
+test.bank <- bank[-index,]
+cat("Attrited customers (rare class): ",table(train.bank$Attrition_Flag)[2])
+cat("Existing customers (maj class): ",table(train.bank$Attrition_Flag)[1])
+
+# Fitting AdaBoost to the Training set and keep track of variable importance
+bank.boost <- boosting(Attrition_Flag ~ ., data = train.bank, boos = TRUE)
+print(bank.boost)
+
+# Predicting the Test set results
+pred <- predict(bank.boost, newdata = test.bank, type="response")
+pred$confusion
+pred$prob[,2] >0.5
+
+# Extract information about variable selection
+# Extract the variable selection information for each boosting iteration
+var_selection <- bank.boost$importance
+
+# Sort the variable importance in descending order
+sorted_variable_importance <- sort(var_selection, decreasing = TRUE)
+
+# Convert variable importance to a data frame
+variable_importance_df <- data.frame(Variable = names(sorted_variable_importance),
+                                     Importance = sorted_variable_importance,
+                                     row.names = NULL)
+
+# Print the variable importance as a column
+print(variable_importance_df)
+
+# CV splitting the dataset into the Training set and Test set
+set.seed(1234)
+bank.boostcv <- boosting.cv(Attrition_Flag ~ ., data = bank, boos = TRUE,
+                            v=10)
+
+# Confusion matrix for the best iteration
+bank.boostcv$confusion
+
+#-------------------------------------------------------------------------------
+# SECOND TRY WITH RANDOM FOREST
+bank.rf <- randomForest(Attrition_Flag ~ ., data = train.bank, ntree = 500,
+                        seed=123, importance = TRUE)
+
+# Predicting the Test set results
+pred <- predict(bank.rf, newdata = test.bank, type="response")
+pred
+pred<- pred >0.5
+confusion_matrix<- table(pred, test.bank$Attrition_Flag)
+confusion_matrix
+
+print(bank.rf)
+
+# Variable importance plot
+varImpPlot(bank.rf, sort = TRUE, n.var = 10, main = "Variable Importance")
+
+# Variable importance table
+# Extract variable importance
+variable_importance <- importance(bank.rf, type = 2)
+
+# Create a data frame for variable names and importance values
+variable_importance_df <- data.frame(
+  Variable = row.names(variable_importance),
+  Importance = as.numeric(variable_importance),
+  row.names = NULL
+)
+
+# Sort the data frame by Importance in descending order
+variable_importance_df <- variable_importance_df[order(-variable_importance_df$Importance), ]
+
+# Print the variable importance as a data frame
+print(variable_importance_df)
+
 
 # Plotting the tree
 plot(bank.rf, main = "Random Forest")
